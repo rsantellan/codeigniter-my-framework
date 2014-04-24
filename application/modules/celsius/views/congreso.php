02f28d796302e5255d2e9f14378187c9891eabdb
rsantellan@gmail.com <rsantellan@gmail.com@cad0f972-c4b9-fa69-6a77-906b6385f995>
2014-04-24 11:00:35 +0000

    Se agrega el nombre.

--- application/modules/celsius/views/congreso.php
+++ application/modules/celsius/views/congreso.php
@@ -1,5 +1,6 @@
 <div class="content_site content_internas_seccion_iniciada">
   <?php echo $this->load->view('navegacionprivada') ?>
+  <h3><?php echo $object->getName();?></h3>
   <ul class="content">
     <?php 
             $imgType = 3;
