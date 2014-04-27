64d051d10a9429e84e73d665be845f3fe8f48ef4
rsantellan@gmail.com <rsantellan@gmail.com@cad0f972-c4b9-fa69-6a77-906b6385f995>
2014-04-27 22:11:59 +0000

    Basic sumuy data.

---
+++ basicSumuyData.sh
@@ -0,0 +1,46 @@
+rm *.sql
+rm *.txt
+rm captcha/* -rf
+rm assets/celsius -rf
+rm assets/feu -rf
+find . -name ".svn" -exec rm -rf {} \;
+rm assets/cache/* -rf
+rm assets/css/roche -rf
+rm assets/js/roche -rf
+rm assets/images/roche -rf
+rm -rf application/logs/log*
+rm -rf application/libraryNoSeUsa
+rm -rf application/modules/banners
+rm -rf application/modules/basiclink
+rm -rf application/modules/blog
+rm -rf application/modules/categorias
+rm -rf application/modules/categories
+rm -rf application/modules/celsius
+rm -rf application/modules/clubes
+rm -rf application/modules/congress
+rm -rf application/modules/departamentos
+rm -rf application/modules/documents
+rm -rf application/modules/events
+rm -rf application/modules/feu
+rm -rf application/modules/galerias
+rm -rf application/modules/historicosadmin
+rm -rf application/modules/jornadas
+rm -rf application/modules/jornadatemas
+rm -rf application/modules/laboratorios
+rm -rf application/modules/news
+rm -rf application/modules/presentations
+rm -rf application/modules/products
+rm -rf application/modules/proyectos
+rm -rf application/modules/pruebas
+rm -rf application/modules/radios
+rm -rf application/modules/roche
+rm -rf application/modules/sitio
+rm -rf application/modules/studycases
+rm -rf application/modules/tags
+rm -rf application/modules/testmulti
+rm -rf application/modules/trabajaconnosotros
+rm -rf application/modules/veterinariosadmin
+rm -rf application/modules/welcome
+rm application/views/celsius_*
+rm application/views/feu_layout.php application/views/layout.php application/views/roche_layout.php
+
