cdfaf197ad8d00c5bf40b47fb0580136cb4459ae
rsantellan@gmail.com <rsantellan@gmail.com@cad0f972-c4b9-fa69-6a77-906b6385f995>
2014-04-30 06:43:40 +0000

    Se agrega esto : https://github.com/2b3ez/FileManager4TinyMCE
    Para subir imagenes desde el tiny.. Hay que hacerlo funcionar.

--- assets/admin/js/adminManager.js
+++ assets/admin/js/adminManager.js
@@ -101,11 +101,11 @@ adminManager.prototype = {
               "advlist autolink lists link image charmap print preview hr anchor pagebreak",
               "searchreplace wordcount visualblocks visualchars code fullscreen",
               "insertdatetime media nonbreaking save table contextmenu directionality",
-              "emoticons template paste textcolor"
+              "emoticons template paste textcolor filemanager"
           ],
           toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
           toolbar2: "print preview media | forecolor backcolor emoticons",
-          image_advtab: true,
+		  image_advtab: true,
           forced_root_block : "",
           force_br_newlines : true,
           force_p_newlines : false
